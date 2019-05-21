1eb52fbc2257c177a1e9e7e5d2a6b347ab2e86ff
Pavel Lang <langpavel@phpskelet.org>
2019-05-21 22:51:38 +0200

    wip

--- packages/ts-source-writer/src/template-tag/identifier.ts
+++ packages/ts-source-writer/src/template-tag/identifier.ts
@@ -4,7 +4,6 @@ import { SourceResolver } from './source-resolver';
 export type IdentifierConfig = {
   name?: string;
   possibleNames?: string[];
-  noRename?: boolean;
   isExported?: boolean;
 };
 
@@ -13,7 +12,6 @@ export type IdentifierOptions = IdentifierConfig | string | string[];
 export class Identifier extends SourceFragment implements IdentifierConfig {
   name?: string;
   possibleNames?: string[];
-  noRename?: boolean;
   isExported?: boolean;
   resolvedName?: string;
 
@@ -21,13 +19,11 @@ export class Identifier extends SourceFragment implements IdentifierConfig {
     super();
     if (typeof config === 'string') {
       this.name = config;
-      this.noRename = true;
     } else if (typeof config !== 'undefined') {
       if (Array.isArray(config)) {
         this.possibleNames = config;
       } else {
         this.name = config.name;
-        this.noRename = config.noRename;
       }
     }
   }
